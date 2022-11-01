bffaf4efc461bb911d613f0a1ee4ac595a0425b8
DemetriusJayme <115664765+DemetriusJayme@users.noreply.github.com>
2022-11-01 12:44:25 -0300

    logica pronta

--- js/index.js
+++ js/index.js
@@ -2,41 +2,73 @@
 
 console.log("JS connected"); // <== just a quick check up to make sure js doc is connected properly
 
-// TO GET THE ELEMENT FROM DOM YOU CAN USE "getElementById"
-let Jogador1 = document.getElementById("InputName1");
+btnPar.onclick = () => {
+  console.log("cliquei no botão par");
+  resultGame("par");
+};
 
-console.log(Jogador1); // <== what can you see in browser's console
+btnImpar.onclick = () => {
+  console.log("cliquei no botão impar");
+  resultGame("impar");
+};
+
+//Calcula o resultado do jogo
+function resultGame(flag) {
+  console.log("entrei na funcao");
+  console.log(flag);
+
+  const qtdeDedosFirst = document.getElementById("inputQtdeFirst").value;
+  const qtdeDedosSecond = document.getElementById("inputQtdeSecond").value;
+  const total = parseInt(qtdeDedosFirst) + parseInt(qtdeDedosSecond);
+
+  console.log(qtdeDedosFirst);
+  console.log(qtdeDedosSecond);
+
+  //const total = sum(qtdeDedosFirst.value, qtdeDedosSecond.value);
+
+  console.log(total);
+
+  if (total % 2 == 0) {
+    console.log("eh par");
+  } else {
+    console.log("eh impar");
+  }
+}
+
+// TO GET THE ELEMENT FROM DOM YOU CAN USE "getElementById"
+//let Jogador1 = document.getElementById("InputName1");
+
+//console.log(Jogador1); // <== what can you see in browser's console
 
 // TO ADD TEXT TO DOM USE "innerHTML"
-jogador1.innerHTML = "I'm a dog";
+//jogador1.innerHTML = "I'm a dog";
 
 // set the HTML content of "otherElement" to "I'm a cat"
-otherElement.innerHTML = theCatDiv.innerHTML;
+//otherElement.innerHTML = theCatDiv.innerHTML;
 
 //capturando todos os elementos de html
-const startScreen = document.getElementById("startScreen");
-const inputName = document.getElementById("inputName");
-const gameScreen = document.getElementById("gameScreen");
-const gameScore = document.getElementById("gameScore");
-const playerName = document.getElementById("name");
-const board = document.getElementById("board");
-const chances = 5;
+//const startScreen = document.getElementById("startScreen");
+//const inputName = document.getElementById("inputName");
+//const gameScreen = document.getElementById("gameScreen");
+//const gameScore = document.getElementById("gameScore");
+//const playerName = document.getElementById("name");
+//const board = document.getElementById("board");
+//const chances = 5;
 
 //adicionar o event listener do submit
-addEventListener("submit", () => {
-  //instanciar a minha classe
-  const game = new ParImparGame(inputName.value, chances);
+//addEventListener("submit", () => {
+//instanciar a minha classe
+//const game = new ParImparGame(inputName.value, chances);
 
-  // startScreen desapareça
-  startScreen.classList.add("hide");
+// startScreen desapareça
+//startScreen.classList.add("hide");
 
-  // mostrar o gameScore
-  //gameScore.className = "show";
+// mostrar o gameScore
+//gameScore.className = "show";
 
-  //game.renderDeck();
+//game.renderDeck();
 
-  //settingUpGame(game);
-});
+//settingUpGame(game);;
 
 /*
 function settingUpGame(game) {
